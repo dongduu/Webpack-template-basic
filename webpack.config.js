4b4c3748797ed7becd7d19cc6da0c003bf67367d
dongduu <4012popo@naver.com>
2021-12-30 23:25:29 +0900

    feat: Create project

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+// import
+const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+// exports
+module.exports = {
+    entry: "./main.js",
+
+    output: {
+        // path: path.resolve(__dirname, 'dist'),
+        // filename: 'main.js',
+        clean: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+
+    // 번들링 후 결과물의 처리 방식 등 다양한 플러그인을 설정
+    plugins: [
+        new HtmlPlugin({
+            template: './index.html'
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'static' }
+            ]
+        })
+    ],
+
+    devServer: {
+        host: 'localhost'
+    }
+}
